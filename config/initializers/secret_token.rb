ebe1f0970192ea48e5f2bff3dab16edbfbad3370
Nitish <nitish@fedora16lxde.com>
2012-11-22 18:52:09 +0530

    secret token for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBlog::Application.config.secret_token = '42b186a36f19fa8711d3fb8b30eca06f6d258b0ada605caf187f49bd377667096910ac12489d3e335a8e2204e70c6ebc68a738a42ef9879f5811b7f4566bbd46'
