6846c76276b2829c2a0c1cbcf80542ff76e349b8
Nitish <nitish@fedora16lxde.com>
2012-11-24 01:45:22 +0530

    fixed bugs, authetication for comments

--- app/controllers/user_comments_controller.rb
+++ app/controllers/user_comments_controller.rb
@@ -1,19 +1,29 @@
 class UserCommentsController < ApplicationController
-    def create
+    before_filter :get_blog_post
+    before_filter :check_authorisation, only: :destroy
+
+    def get_blog_post
         @blog_post = BlogPost.find(params[:blog_post_id])
+    end
+
+    def check_authorisation
+        unless @blog_post.user_id == current_user.id
+          redirect_to blog_post_path(@blog_post), notice: 'You can only delete comments on your own blog'
+        end
+    end
+
+    def create
         @user_comment = @blog_post.user_comments.create(params[:user_comment])
         redirect_to blog_post_path(@blog_post)
     end
 
     def destroy
-        @blog_post = BlogPost.find(params[:blog_post_id])
         @user_comment = @blog_post.user_comments.find(params[:id])
         @user_comment.destroy
         redirect_to blog_post_path(@blog_post)
     end
 
     def index
-        @blog_post = BlogPost.find(params[:blog_post_id])
         @user_comments = @blog_post.user_comments
         respond_to do |format|
           format.html # index.html.erb
