7d7db444a0e355d1c586397e6104e6d7f0249aa2
Nitish <nitish@genii.in>
2013-01-15 20:54:35 +0530

    added filters and sorts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,11 +22,99 @@ $(document).ready(function() {
 			
 		   var url = $('.pagination .next_page').attr('href');
 		   if(url && $(window).scrollTop() + $(window).height() > $(document).height() - 10) {
-		   	   console.log('scroll');
+		   	   //console.log('scroll');
 		   	   $('.pagination').text('fetching more records..');
 		       $.getScript(url);
 		   }
 		});
 	}
+
+	$('#created').click(function(){
+		var sorting_direction = $(this).attr('class');
+        
+        if(sorting_direction === 'u') {
+            $(this).html('v');
+        } else {
+            $(this).html('^');
+        }
+        $(this).toggleClass('u d');
+        
+        
+        $bps = $('#blog-posts').children();
+
+       	$bps.sort(function(a, b){
+       		//console.log( Date.parse($(a).find('#created').text()) - Date.parse($(b).find('#created').text()) );
+       		if(sorting_direction === 'u') {
+        		return Date.parse($(a).find('#created').text()) - Date.parse($(b).find('#created').text());
+        	} else {
+        		return Date.parse($(b).find('#created').text()) - Date.parse($(a).find('#created').text());
+        	}
+        });
+
+        $('#blog-posts').empty();
+        $('#blog-posts').prepend($bps);
+	});
+
+	$('#modified').click(function(){
+		var sorting_direction = $(this).attr('class');
+        
+        if(sorting_direction === 'u') {
+            $(this).html('v');
+        } else {
+            $(this).html('^');
+        }
+        $(this).toggleClass('u d');
+        
+        
+        $bps = $('#blog-posts').children();
+
+       	$bps.sort(function(a, b){
+       		//console.log( Date.parse($(a).find('#modified').text()) - Date.parse($(b).find('#modified').text()) );
+       		if(sorting_direction === 'u') {
+        		return Date.parse($(a).find('#modified').text()) - Date.parse($(b).find('#modified').text());
+        	} else {
+        		return Date.parse($(b).find('#modified').text()) - Date.parse($(a).find('#modified').text());
+        	}
+        });
+
+        $('#blog-posts').empty();
+        $('#blog-posts').prepend($bps);
+	});
+
+	filterFunction = function() {
+		search_term = $.trim($('#global_search').val()).toLowerCase();
+		user_filter = $.trim($('#user_search').val()).toLowerCase();
+		
+		$('.blog-post').each(function() {
+			flag = true;
+			
+			if(user_filter.length) {
+				if(($(this).find('#author').text().toLowerCase()).indexOf(user_filter) < 0) {
+					flag = false;
+				}
+			}
+
+			if(search_term.length) {
+				//console.log($(this).find('.post-header h2').html().toLowerCase())
+				if( (($(this).find('.post-header h2').text().toLowerCase()).indexOf(search_term) < 0)
+					&& (($(this).find('.post-content').text().toLowerCase()).indexOf(search_term) < 0) ) {
+					flag = false;
+				}
+			}
+
+
+			if(flag) {
+				$(this).show();
+			} else {
+				$(this).hide();
+			}
+		});
+	}
+
+
+	$('#global_search, #user_search').keyup(function(evt) {
+		filterFunction();
+		//console.log('called');
+	});
 	
 });
